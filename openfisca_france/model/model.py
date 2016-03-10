4d2ed60d5562ef200c4651573a90dcf42c633b2e
Mael Thomas <mael.thomas@data.gouv.fr>
2016-03-10 13:19:32 +0100

    Move back coefficient_proratisation to allegements.py

--- openfisca_france/model/model.py
+++ openfisca_france/model/model.py
@@ -30,7 +30,6 @@ from .prelevements_obligatoires.prelevements_sociaux.contributions_sociales impo
 
 from .prelevements_obligatoires.prelevements_sociaux.cotisations_sociales import (  # noqa analysis:ignore
     allegements,
-    temps_partiel,
     apprentissage,
     exonerations,
     # penalites,
